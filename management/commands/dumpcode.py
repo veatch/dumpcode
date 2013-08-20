8006aa5ff46e6c91db57e1e4314606680b53ff30
Jason Veatch <jason.veatch@cmgdigital.com>
2013-08-20 19:06:51 -0400

    Split handle method into a few different methods. Did some re-naming and commenting.

--- management/commands/dumpcode.py
+++ management/commands/dumpcode.py
@@ -1,17 +1,14 @@
 import gzip
 import os
-import sys
-import traceback
 import zipfile
 
 from django.conf import settings
-from django.core import serializers
 from django.core.management.base import BaseCommand
-from django.core.management.color import no_style
 from django.core.serializers import base
 from django.db import DEFAULT_DB_ALIAS, models
 from django.utils import simplejson
 from django.utils.encoding import smart_unicode
+from django.utils.itercompat import product
 
 try:
     import bz2
@@ -19,52 +16,119 @@ try:
 except ImportError:
     has_bz2 = False
 
+class SingleZipReader(zipfile.ZipFile):
+    def __init__(self, *args, **kwargs):
+        zipfile.ZipFile.__init__(self, *args, **kwargs)
+        if len(self.namelist()) != 1:
+            raise ValueError("Zip-compressed fixtures must contain one file.")
+
+    def read(self):
+        return zipfile.ZipFile.read(self, self.namelist()[0])
+
+compression_types = {
+    None: open,
+    'gz': gzip.GzipFile,
+    'zip': SingleZipReader
+}
+if has_bz2:
+    compression_types['bz2'] = bz2.BZ2File
+
 INDENT = '    '
+
 
 class Command(BaseCommand):
     """
     Usage: django-admin.py dumpcode fixture.json
-    This will read fixture.json, translate json representation of objects into
-    python to create those objects, and print a method with the necessary imports
-    and python creation statements.
+    This will read the given fixture, translate json representation of objects into
+    python, and print a method that will create all objects from fixture.
+    Based on loaddata management command (django/core/management/commands/loaddata.py)
     """
+    help = ''
     def handle(self, *fixture_labels, **options):
-        using = options.get('database')
-
-        self.style = no_style()
 
         if not len(fixture_labels):
             self.stderr.write(
-                self.style.ERROR("No database fixture specified. Please provide the path of at least one fixture in the command line.\n")
+                "No database fixture specified. Please provide the path of at least one fixture in the command line.\n"
             )
             return
 
-        verbosity = int(options.get('verbosity'))
-        show_traceback = options.get('traceback')
+        self.verbosity = int(options.get('verbosity'))
 
-        # Keep a count of the installed objects and fixtures
-        fixture_count = 0
-        loaded_object_count = 0
-        fixture_object_count = 0
+        for fixture_label in fixture_labels:
+            translator = JsonTranslator(fixture_label)
+            self.setup_name_compression_dir(fixture_label.split('.'))
 
-        humanize = lambda dirname: "'%s'" % dirname if dirname else 'absolute path'
+            fixture_files = self.find_fixtures(fixture_label, self.fixture_dirs)
 
-        class SingleZipReader(zipfile.ZipFile):
-            def __init__(self, *args, **kwargs):
-                zipfile.ZipFile.__init__(self, *args, **kwargs)
-                if settings.DEBUG:
-                    assert len(self.namelist()) == 1, "Zip-compressed fixtures must contain only one file."
-            def read(self):
-                return zipfile.ZipFile.read(self, self.namelist()[0])
+            for full_path, compression_format in fixture_files:
+                try:
+                    fixture = open_fixture(full_path, compression_format)
+                except IOError:
+                    if self.verbosity >= 2:
+                        self.stdout.write("Error opening fixture %s.\n" % full_path)
+                else:
+                    for obj_json in simplejson.load(fixture):
+                        translator.translate_object(obj_json)
+                finally:
+                    fixture.close()
+                    translator.out()
 
-        compression_types = {
-            None:   open,
-            #'gz':   gzip.GzipFile,
-            #'zip':  SingleZipReader
-        }
-        #if has_bz2:
-            #compression_types['bz2'] = bz2.BZ2File
+    def find_fixtures(self, fixture_label, fixture_dirs):
+        """
+        Search for fixture_label in fixture_dirs. Returns list of tuples.
+        In each tuple first element is fixture's full path, second element
+        is compression format.
+        """
+        fixtures = []
+        for fixture_dir in fixture_dirs:
+            if self.verbosity >= 2:
+                self.stdout.write("Checking %s for fixtures...\n" % humanize(fixture_dir))
 
+            label_found = False
+            for combo in product(['json'], self.compression_formats):
+                format, compression_format = combo
+                file_name = '.'.join(
+                    p for p in [
+                        self.fixture_name, format, compression_format
+                    ]
+                    if p
+                )
+
+                if self.verbosity >= 3:
+                    self.stdout.write("Trying %s for %s fixture '%s'...\n" % \
+                        (humanize(fixture_dir), file_name, self.fixture_name))
+                full_path = os.path.join(fixture_dir, file_name)
+                try:
+                    fixture = open_fixture(full_path, compression_format)
+                except IOError:
+                    if self.verbosity >= 2:
+                        self.stdout.write("No %s fixture '%s' in %s.\n" % \
+                            (format, self.fixture_name, humanize(fixture_dir)))
+                else:
+                    try:
+                        if label_found:
+                            self.stderr.write("Multiple fixtures named '%s' in %s. Aborting.\n" %
+                                (self.fixture_name, humanize(fixture_dir)))
+                            return
+
+                        fixtures.append((full_path, compression_format))
+
+                        if self.verbosity >= 2:
+                            self.stdout.write("Installing %s fixture '%s' from %s.\n" % \
+                                (format, self.fixture_name, humanize(fixture_dir)))
+
+
+                        label_found = True
+                    finally:
+                        fixture.close()
+
+        return fixtures
+
+    def setup_name_compression_dir(self, parts):
+        """
+        Given parts of fixture label, set compression_formats, fixture_name,
+        and fixture_dirs.
+        """
         app_module_paths = []
 
         for app in models.get_apps():
@@ -78,139 +142,49 @@ class Command(BaseCommand):
 
         app_fixtures = [os.path.join(os.path.dirname(path), 'fixtures') for path in app_module_paths]
 
-        try:
-            for fixture_label in fixture_labels:
-                dumper = CodeDumper(fixture_label)
-                parts = fixture_label.split('.')
+        if len(parts) > 1 and parts[-1] in compression_types:
+            self.compression_formats = [parts[-1]]
+            parts = parts[:-1]
+        else:
+            self.compression_formats = compression_types.keys()
 
-                if len(parts) > 1 and parts[-1] in compression_types:
-                    compression_formats = [parts[-1]]
-                    parts = parts[:-1]
-                else:
-                    compression_formats = compression_types.keys()
+        if len(parts) == 1:
+            self.fixture_name = parts[0]
+        else:
+            self.fixture_name, format = '.'.join(parts[:-1]), parts[-1]
 
-                if len(parts) == 1:
-                    fixture_name = parts[0]
-                    formats = serializers.get_public_serializer_formats()
-                else:
-                    fixture_name, format = '.'.join(parts[:-1]), parts[-1]
-                    if format in serializers.get_public_serializer_formats():
-                        formats = [format]
-                    else:
-                        formats = []
+        if os.path.isabs(self.fixture_name):
+            self.fixture_dirs = [self.fixture_name]
+        else:
+            self.fixture_dirs = app_fixtures + list(settings.FIXTURE_DIRS) + ['']
 
-                if formats:
-                    if verbosity >= 2:
-                        self.stdout.write("Loading '%s' fixtures...\n" % fixture_name)
-                else:
-                    self.stderr.write(
-                        self.style.ERROR("Problem installing fixture '%s': %s is not a known serialization format.\n" %
-                            (fixture_name, format)))
-                    return
-
-                if os.path.isabs(fixture_name):
-                    fixture_dirs = [fixture_name]
-                else:
-                    fixture_dirs = app_fixtures + list(settings.FIXTURE_DIRS) + ['']
-
-                for fixture_dir in fixture_dirs:
-                    if verbosity >= 2:
-                        self.stdout.write("Checking %s for fixtures...\n" % humanize(fixture_dir))
-
-                    label_found = False
-                    for combo in [(None, 'json', None)]:
-                        database, format, compression_format = combo
-                        file_name = '.'.join(
-                            p for p in [
-                                fixture_name, database, format, compression_format
-                            ]
-                            if p
-                        )
-
-                        if verbosity >= 3:
-                            self.stdout.write("Trying %s for %s fixture '%s'...\n" % \
-                                (humanize(fixture_dir), file_name, fixture_name))
-                        full_path = os.path.join(fixture_dir, file_name)
-                        open_method = compression_types[compression_format]
-                        try:
-                            fixture = open_method(full_path, 'r')
-                        except IOError:
-                            if verbosity >= 2:
-                                self.stdout.write("No %s fixture '%s' in %s.\n" % \
-                                    (format, fixture_name, humanize(fixture_dir)))
-                        else:
-                            try:
-                                if label_found:
-                                    self.stderr.write(self.style.ERROR("Multiple fixtures named '%s' in %s. Aborting.\n" %
-                                        (fixture_name, humanize(fixture_dir))))
-                                    return
-
-                                fixture_count += 1
-                                objects_in_fixture = 0
-                                loaded_objects_in_fixture = 0
-                                if verbosity >= 2:
-                                    self.stdout.write("Installing %s fixture '%s' from %s.\n" % \
-                                        (format, fixture_name, humanize(fixture_dir)))
-
-                                # python/dj shell, invoke directly so registry isn't issue
-                                #json_d = serializers.get_deserializer('json')
-
-                                # just like serializers.python.Deserializer, but after Model:
-
-                                for obj_json in simplejson.load(fixture):
-                                    objects_in_fixture += 1
-                                    dumper.dump(obj_json)
-
-                                loaded_object_count += loaded_objects_in_fixture
-                                fixture_object_count += objects_in_fixture
-                                label_found = True
-                            finally:
-                                fixture.close()
-                                dumper.out()
-
-                            # If the fixture we loaded contains 0 objects, assume that an
-                            # error was encountered during fixture loading.
-                            if objects_in_fixture == 0:
-                                self.stderr.write(
-                                    self.style.ERROR("No fixture data found for '%s'. (File format may be invalid.)\n" %
-                                        (fixture_name)))
-                                return
-
-        except (SystemExit, KeyboardInterrupt):
-            raise
-        except Exception:
-            if show_traceback:
-                traceback.print_exc()
-            else:
-                self.stderr.write(
-                    self.style.ERROR("Problem installing fixture '%s': %s\n" %
-                         (full_path, ''.join(traceback.format_exception(sys.exc_type,
-                             sys.exc_value, sys.exc_traceback)))))
-            return
-
-
-class CodeDumper(object):
+class JsonTranslator(object):
     """
     For a given fixture, accepts json representations of objects and translates to python.
     Prints a method with necessary imports and python creation statements.
+    The translate_object method is based on Deserializer in django/core/serializers/python.py
     """
-    def __init__(self, fixture_name=None):
-        self.imports = []
-        self.code = []
+    def __init__(self, fixture_name):
+        self.import_statements = []
+        self.object_setup_statements = []
         self.db = DEFAULT_DB_ALIAS
-        self.fixture_name = ''
-        if fixture_name:
-            self.fixture_name = fixture_name
+        self.fixture_name = fixture_name
 
-    # todo: rename this so it's clearer that one object is being dealt with
-    def dump(self, obj_json):
+    def translate_object(self, obj_json):
+        """
+        Given json representation of object, make python strings to create object.
+        Add import string for object's model to import_statements (if it's not
+        already there), add string to create object to object_setup_statements,
+        and add strings to create many-to-many relations to object_setup_statements.
+        Based on Deserializer in django/core/serializers/python.py
+        """
         Model = _get_model(obj_json["model"])
         data = {Model._meta.pk.attname : Model._meta.pk.to_python(obj_json["pk"])}
         m2m_data = {}
         imprt = 'from %s import %s' % (Model.__module__, Model._meta.object_name)
-        if imprt not in self.imports:
-            self.imports.append(imprt)
-        #populate kwargs with dict (no m2m or fks)
+        if imprt not in self.import_statements:
+            self.import_statements.append(imprt)
+
         # Handle each field
         for (field_name, field_value) in obj_json["fields"].iteritems():
             if isinstance(field_value, str):
@@ -258,19 +232,28 @@ class CodeDumper(object):
         if add_m2m:
             obj_str = 'obj = '
 
-        self.code.append('%s%s.objects.create(**%s)' % (obj_str, Model._meta.object_name, data))
+        self.object_setup_statements.append('%s%s.objects.create(**%s)' % (obj_str, Model._meta.object_name, data))
         if add_m2m:
-            self.code.extend(add_m2m)
-            #self.code.append('obj.save()')
+            self.object_setup_statements.extend(add_m2m)
 
     def out(self):
+        """
+        Print out python strings made by translate_object method as a self-contained method.
+        """
         print
         print 'def create_%s_objects():' % self.fixture_name
-        for imprt in self.imports:
+        for imprt in self.import_statements:
             print '%s%s' % (INDENT, imprt)
         print
-        for c in self.code:
+        for c in self.object_setup_statements:
             print '%s%s' % (INDENT, c)
+
+
+humanize = lambda dirname: "'%s'" % dirname if dirname else 'absolute path'
+
+def open_fixture(fixture, compression_format):
+    open_method = compression_types[compression_format]
+    return open_method(fixture, 'r')
 
 def _get_model(model_identifier):
     """
